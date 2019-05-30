9e22ca8f95eb167e00881a1b2deb4bec83196f01
Dwash <45184923+DMwashington@users.noreply.github.com>
2019-05-30 09:51:43 -0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var exphbs = require("express-handlebars");
+var mongoose = require("mongoose");
+
+var app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.engine("handlebars", exphbs({defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
+require("./controllers/news-routes.js")(app);
+
+var PORT = process.env.PORT || 8000;
+
+mongoose.Promise = Promise;
+
+if(process.env.MONGODB_URI)
+	mongoose.connect(process.env.MONGODB_URI);
+else
+	mongoose.connect("mongodb://localhost/news-scraper");
+
+app.listen(PORT, function() {
+	console.log("App listening on port " + PORT);
+});
